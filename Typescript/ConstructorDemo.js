e4971a7706c7d8b1ceaaafed080a89090b8baf6f
kokaneva <kokane.vina@gmail.com>
2022-12-29 15:01:14 +0530

    instance methods

--- Typescript/ConstructorDemo.js
+++ Typescript/ConstructorDemo.js
@@ -10,6 +10,24 @@ var BankAccount = /** @class */ (function () {
         this.accType = type;
         this.balance = bal;
     }
+    BankAccount.prototype.deposit = function (amount) {
+        if (amount > 0)
+            this.balance = this.balance + amount;
+        else
+            throw new Error("Invalid Deposit Amount");
+        return this.balance;
+    };
+    BankAccount.prototype.withdraw = function (amount) {
+        if (amount <= this.balance)
+            this.balance = this.balance - amount;
+        else
+            throw new Error("Invalid Withdraw Amount");
+        return this.balance;
+    };
+    BankAccount.prototype.getDetails = function () {
+        // return "Account Num:"+this.accNum +" Customer Id:"+ this.custId+" Type:"+ this.accType+" Balance:"+this.balance;
+        return "Accunt Num:".concat(this.accNum, " Cust Id:").concat(this.custId, " Type:").concat(this.accType, " Balance:").concat(this.balance);
+    };
     return BankAccount;
 }());
 //document.write("<b>Message on broswer</b>");
@@ -19,6 +37,10 @@ console.log(account1.accNum);
 console.log(account1.custId);
 console.log(account1.accType);
 console.log(account1.balance);
+account1.balance = 56000; // state change
+var b = account1.withdraw(4000);
+console.log("balance after withdraw:" + b);
+console.log("balance after withdraw:" + account1.balance);
 console.log(account1);
 console.log(JSON.stringify(account1));
 var account2 = new BankAccount(343434, 1111, "Savings", 4000);
@@ -26,3 +48,8 @@ console.log(account2.accNum);
 console.log(account2.custId);
 console.log(account2.accType);
 console.log(account2.balance);
+var b2 = account2.deposit(10000);
+console.log("balance after depsit:" + b2);
+console.log("balance after depsit:" + account2.balance);
+console.log(account1.getDetails());
+console.log(account2.getDetails());
--- Typescript/ConstructorDemo.ts
+++ Typescript/ConstructorDemo.ts
@@ -1,15 +1,37 @@
 class BankAccount{
-    accNum:number;
-    custId:number;
-    accType:string;
-    balance:number;
+    accNum:number
+    custId:number
+    accType:string;balance:number;
 
 //Multiple constructor implementations are not allowed.
-    constructor(accNum:number=0,custId:number=0,type:string="savings",bal:number=0){
+    constructor(accNum=0,custId=0,type="savings",bal=0){
         this.accNum=accNum;
         this.custId=custId;
         this.accType=type;
         this.balance=bal;
+    }
+
+    deposit(amount:number):number
+    {
+        if(amount>0)
+            this.balance=this.balance+amount;
+        else
+            throw new Error("Invalid Deposit Amount");
+            
+        return this.balance;  
+    }
+    withdraw(amount:number):number {
+        if(amount<=this.balance)
+            this.balance=this.balance-amount;
+        else
+            throw new Error("Invalid Withdraw Amount");
+        return this.balance;
+    }
+
+    getDetails():string{
+       // return "Account Num:"+this.accNum +" Customer Id:"+ this.custId+" Type:"+ this.accType+" Balance:"+this.balance;
+    
+        return `Accunt Num:${this.accNum} Cust Id:${this.custId} Type:${this.accType} Balance:${this.balance}`
     }
 }
 
@@ -20,6 +42,12 @@ console.log(account1.accNum);
 console.log(account1.custId);
 console.log(account1.accType);
 console.log(account1.balance);
+account1.balance=56000; // state change
+
+let b=account1.withdraw(4000);
+console.log("balance after withdraw:"+b);
+console.log("balance after withdraw:"+account1.balance);
+
 console.log(account1);
 console.log(JSON.stringify(account1));
 let account2=new BankAccount(343434,1111,"Savings",4000);
@@ -27,3 +55,10 @@ console.log(account2.accNum);
 console.log(account2.custId);
 console.log(account2.accType);
 console.log(account2.balance);
+
+let b2=account2.deposit(10000);
+console.log("balance after depsit:"+b2);
+console.log("balance after depsit:"+account2.balance);
+
+console.log(account1.getDetails());
+console.log(account2.getDetails());
