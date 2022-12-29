ebf3474c59b45a42719708552bb106e12adbf0f5
kokaneva <kokane.vina@gmail.com>
2022-12-29 13:10:03 +0530

    typescript state change

--- Typescript/Demo.js
+++ Typescript/Demo.js
@@ -13,11 +13,25 @@ myName = new String("Pooja");
 var BankAccount = /** @class */ (function () {
     function BankAccount() {
     }
+    BankAccount.prototype.test = function () {
+        return "hi";
+    };
     return BankAccount;
 }());
+//document.write("<b>Message on broswer</b>");
 var account1;
 account1 = new BankAccount(); // implicit call goes to constructor
 console.log(account1.accNum);
 console.log(account1.custId);
 console.log(account1.accType);
 console.log(account1.balance);
+account1.accNum = 67799; // state change
+account1.custId = 67;
+account1.accType = "current";
+account1.balance = 4000;
+console.log(account1.accNum);
+console.log(account1.custId);
+console.log(account1.accType);
+console.log(account1.balance);
+console.log(account1);
+console.log(JSON.stringify(account1));
--- Typescript/Demo.ts
+++ Typescript/Demo.ts
@@ -18,8 +18,10 @@ class BankAccount{
         custId:number;
         accType:string;
         balance:number;
+
 }
 
+//document.write("<b>Message on broswer</b>");
 let account1:BankAccount;
 
 account1=new BankAccount(); // implicit call goes to constructor
@@ -27,4 +29,16 @@ console.log(account1.accNum);
 console.log(account1.custId);
 console.log(account1.accType);
 console.log(account1.balance);
+
+account1.accNum=67799;   // state change
+account1.custId=67;
+account1.accType="current";
+account1.balance=4000;
+
+console.log(account1.accNum);
+console.log(account1.custId);
+console.log(account1.accType);
+console.log(account1.balance);
+console.log(account1);
+console.log(JSON.stringify(account1));
 
