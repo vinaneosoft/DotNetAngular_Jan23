bd9bf7ac37b224a05089f43a028461f1d1916586
kokaneva <kokane.vina@gmail.com>
2023-01-02 16:23:45 +0530

    Generics

--- Typescript/Generics/GenericDemo2.ts
+++ Typescript/Generics/GenericDemo2.ts
@@ -1,5 +1,4 @@
 
-
 class MyObject<T>{
     private object:T;
 
@@ -14,10 +13,33 @@ class MyObject<T>{
     }
 
 }
-
 let myobject1=new MyObject<Number>(45);
  myobject1.ob
 
-
  let myobject2=new MyObject<String>("hello");
  myobject2.ob
+
+
+interface Emp<T>{
+    id:T;
+}
+
+class Trainer implements Emp<Number>{
+    id:Number;
+}
+class Developer implements Emp<String>{
+    id:String
+}
+
+let t1=new Trainer();
+t1.id=67
+let t2=new Trainer();
+t1.id=89
+let t3=new Trainer();
+t1.id=67
+
+let d1=new Developer();
+d1.id="D2";
+
+
+
