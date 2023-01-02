7ee35aa7ab3816cbf19c44f3aef3c2ab3c6f7420
kokaneva <kokane.vina@gmail.com>
2023-01-02 13:34:00 +0530

    rules of overriding

---
+++ Typescript/Interfaces/Rules.ts
@@ -0,0 +1,29 @@
+class A{
+
+}
+class B extends A{
+
+}
+
+
+class Employee{
+    protected showDetails(n:number):A{
+        return new A();
+    }
+}
+
+class Manager extends Employee{
+    showDetails(m:number):B{
+        return new B();
+    }
+
+}
+
+let manager =new Manager();
+manager.showDetails(67);
+
+// formal parameters of method u can reduce, but can not add more
+// if reducing scope then not allowed, scope if increasing  then allowed
+// if super class method is void then subclass overriding method can return anything
+// if super class method is returning primitive : same should be in subclass overriding  method 
+// if return type is custom then co variants are allowed
