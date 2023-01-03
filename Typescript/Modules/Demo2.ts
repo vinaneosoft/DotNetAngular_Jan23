17e4521a7be38f9c9ce5256d33a91613a7d2ceeb
kokaneva <kokane.vina@gmail.com>
2023-01-03 11:55:33 +0530

    Module

--- Typescript/Modules/Demo2.ts
+++ Typescript/Modules/Demo2.ts
@@ -1,10 +1,15 @@
- import { changeName, companyName } from './Demo1';
+ import { changeName, companyName as cname } from './Demo1';
  import Organization from './Demo1'; // alternate name of Company
-console.log(companyName);
+ import * as neo from "./EmployeeManagement/Employee";
+console.log(cname);
  //companyName="WebWerks";  // value of variable imported from module can not be changed
 
  changeName("Web Werks")
- console.log(companyName);
+ console.log(cname);
 
  let com=new Organization(456);
  com.showId();
+
+ let emp=new neo.Employee();
+
+ let tr=new neo.Trainer();
