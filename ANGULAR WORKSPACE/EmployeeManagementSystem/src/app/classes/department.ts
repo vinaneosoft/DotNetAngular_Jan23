ae02cfb9524712cff7dd2b7e5d3271ee2a08638a
kokaneva <kokane.vina@gmail.com>
2023-01-04 12:47:29 +0530

    1-way data binding

--- ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/classes/department.ts
+++ ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/classes/department.ts
@@ -3,11 +3,12 @@ export class Department {
     deptName:string;
     deptBudget:number;
     deptEstYear:number;
-
-    constructor(id="BB",name="AA",budget=0, year=0){
+    deptImage:string
+    constructor(id="BB",name="AA",budget=0, year=0, image=""){
         this.deptId=id;
         this.deptName=name;
         this.deptBudget=budget;
         this.deptEstYear=year;
+        this.deptImage=image;
     }
 }
--- ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/department/department.component.ts
+++ ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/department/department.component.ts
@@ -9,14 +9,14 @@ import { Department } from '../classes/department';
 export class DepartmentComponent {
 
  deptArray=new Array<Department>();
- deparment=new Department("LD","Learning and Development",100000,2015);
+ deparment=new Department("LD","Learning and Development",100000,2015, "https://picsum.photos/id/560/200/300");
  constructor(){
    
 
   this.deptArray=[
-    new Department("LD","Learning and Development",100000,2015),
-    new Department("JW","Java Web",400000,2000),
-    new Department("DN","Dot Net",500000,2000)
+    new Department("LD","Learning and Development",100000,2015, "https://picsum.photos/id/560/200/300"),
+    new Department("JW","Java Web",400000,2000,  "https://picsum.photos/id/564/200/300"),
+    new Department("DN","Dot Net",500000,2000,  "https://picsum.photos/id/562/200/300")
  ];
  }
 
