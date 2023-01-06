cc0238ec7aa0f5dc9d7867ad993307602ee86d3c
kokaneva <kokane.vina@gmail.com>
2023-01-06 12:29:06 +0530

    routing

--- ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/app-routing.module.ts
+++ ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentComponent } from './department/department.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
 {    path:'home',   component:HomeComponent       },
-{ path:'departments', component:DepartmentComponent }
-
+{ path:'departments', component:DepartmentComponent },
+{ path:'login', component:LoginComponent },
+{ path:'register', component:RegisterComponent }
 ];
 
 @NgModule({
