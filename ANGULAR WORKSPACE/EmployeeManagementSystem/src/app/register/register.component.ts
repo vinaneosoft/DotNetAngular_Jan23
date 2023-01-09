093ac81934d1b268a87024af842f09e3ae5f602c
kokaneva <kokane.vina@gmail.com>
2023-01-09 13:24:42 +0530

    Validation

--- ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/register/register.component.ts
+++ ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/register/register.component.ts
@@ -10,19 +10,22 @@ import { Employee } from '../classes/employee';
 export class RegisterComponent {
 
   passPattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{7,15}$";
+  namePattern="^[A-Za-z]*$";
   registerForm:FormGroup=new FormGroup({});
   employee=new Employee();
   constructor(){
     this.registerForm=new FormGroup({
-      empName:new FormControl("" , [Validators.required, Validators.minLength(2) ]),
+      empName:new FormControl("" , [Validators.required, Validators.minLength(2), Validators.pattern(this.namePattern)]),
       empSalary:new FormControl("",[Validators.required, Validators.min(0)]),
       empGender:new FormControl(""),
       empAddress:new FormControl("",Validators.required),
       departmentId:new FormControl("",Validators.required),
       emailId:new FormControl("",[Validators.required, Validators.email ]),
       password:new FormControl("",[Validators.required, Validators.pattern(this.passPattern) ]), 
-      confirmPassword:new FormControl("",[Validators.required ])
-    });
+      confirmPassword:new FormControl("",[Validators.required]),
+    }
+    ,this.passwordMatch /* custom validator function registered on FormGroup object */
+    );
   }
 
   get deptId(){
@@ -58,6 +61,17 @@ export class RegisterComponent {
     this.employee=this.registerForm.value;
     console.log(this.employee);
   }
+  private passwordMatch(regForm:FormGroup):null | {}{   // special custom validator function
+    console.log("automatically called ");
+     let passNode=regForm.get('password');
+     let cPassNode=regForm.get('confirmPassword');
+     console.log(passNode?.value);
+     console.log(cPassNode?.value);
+      if(passNode?.value==cPassNode?.value)
+        return null
+      else  
+        return {'passMatch':true};
+  }
 }
 /*
   builtin validation method(form control object){
@@ -73,5 +87,6 @@ export class RegisterComponent {
       else 
         null
   }
+
 
 */
