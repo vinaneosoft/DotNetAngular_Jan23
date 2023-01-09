a14a15ac3fae267d053902f42bbb662fed22bccb
kokaneva <kokane.vina@gmail.com>
2023-01-09 16:38:36 +0530

    Template Driven Form

--- ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/cutompipes/prefix.pipe.ts
+++ ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/cutompipes/prefix.pipe.ts
@@ -1,12 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
 @Pipe({
   name: 'prefix'
 })
 export class PrefixPipe implements PipeTransform{
   transform(value: string, ...args: string[]):string {
- 
     return args[0]+"-"+value;
-    
   }
 }
--- ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/login/login.component.ts
+++ ANGULAR WORKSPACE/EmployeeManagementSystem/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from '../classes/employee';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  emp:Employee={
+    emailId:"",
+    empAddress:"",
+    empId:0,
+    empGender:"",
+    empSalary:0,
+    empName:"",
+    password:"",
+    departmentId:""
+  }
+  collectData(logForm:any):void{
+    console.log(logForm.value.emailId);
+    console.log(logForm.value.password);
+    this.emp=logForm.value as Employee;
+    console.log(this.emp);
+  }
 }
